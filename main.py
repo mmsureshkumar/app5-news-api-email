e656f86058e270e24925d76e79a6e66ffd6ac214
smurugas <mmsureshkumar@gmail.com>
2024-08-04 14:52:54 +0530

    Updated the body message and sent a mail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 API_KEY = "7ae9f92f67dd4c598299b3eb94061a36"
 url = f"https://newsapi.org/v2/everything?q=apple&from=2024-08-03&to=2024-08-03&sortBy=popularity&apiKey={API_KEY}"
@@ -8,7 +9,10 @@ request = requests.get(url)
 
 # Get the dictionary
 context = request.json()
-
+body = ""
 for article in context["articles"]:
-    print(article["description"])
-    print(article["title"])
+    if article["title"] is not None:
+        body = body + article["description"] + "\n" + article["title"] + 2 * "\n"
+
+body = body.encode("utf-8")
+send_email(body)
